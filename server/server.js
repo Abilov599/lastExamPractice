8bb33b56155d89c9bd31d80f705e57382e82b098
Abilov Jeyhun <abilovv599@gmail.com>
2023-01-28 00:18:59 +0400

    retried

--- server/server.js
+++ server/server.js
@@ -1,61 +1,79 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
-const port = 3000;
-
 app.use(cors());
-
 app.use(express.json());
+const port = 3000;
 
 mongoose.set("strictQuery", false);
 
 // Declare the Schema of the Mongo model
-const teacherSchema = new mongoose.Schema({
-  teacherName: {
-    type: String,
-    required: true,
+const teacherSchema = new mongoose.Schema(
+  {
+    teacherName: {
+      type: String,
+      required: true,
+    },
+    teacherImg: {
+      type: String,
+      required: true,
+    },
+    teacherType: {
+      type: String,
+      required: true,
+    },
+    teacherDescription: {
+      type: String,
+      required: true,
+    },
   },
-  teacherImg: {
-    type: String,
-    required: true,
-  },
-  teacherType: {
-    type: String,
-    required: true,
-  },
-  teacherDescription: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 //Export the model
 const Teachers = mongoose.model("Teachers", teacherSchema);
 
 app.get("/teachers", async (req, res) => {
-  const data = await Teachers.find();
-  return res.send(data);
-});
-
-app.post("/teachers/", async (req, res) => {
-  const newTeacher = new Teachers(req.body);
-  newTeacher.save();
-  res.status(200).send({ message: "SUCCESS" });
+  try {
+    const data = await Teachers.find();
+    res.send(data);
+  } catch (error) {
+    res.send({ message: "error" });
+  }
 });
 
 app.get("/teachers/:id", async (req, res) => {
   const { id } = req.params;
-  const data = await Teachers.findById(id);
-  return res.send(data);
+  try {
+    const data = await Teachers.findById(id);
+    res.send(data);
+  } catch (error) {
+    res.send({ message: message.error });
+  }
 });
 
 app.delete("/teachers/:id", async (req, res) => {
   const { id } = req.params;
-  const data = await Teachers.findByIdAndDelete(id);
-  return res.send(data);
+  try {
+    await Teachers.findByIdAndDelete(id);
+    res.status(200).send({ message: "SUCCESS" });
+  } catch (error) {
+    res.send({ message: message.error });
+  }
 });
 
+app.post("/teachers", async (req, res) => {
+  try {
+    const newTeachers = new Teachers(req.body);
+    newTeachers.save();
+    res.send({ message: "SUCCESS" });
+  } catch (error) {
+    res.send({ message: message.error });
+  }
+});
+
+// Connect MongoDB
 mongoose.connect(
   "mongodb+srv://abilovv599:cemi2002@cluster0.bh7quof.mongodb.net/?retryWrites=true&w=majority",
   (err) => {
